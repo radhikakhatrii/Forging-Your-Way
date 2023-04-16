0e67c12c02234f3a719137a3895ad572bb668ad7
Radhika Khatri <74490667+radhikakhatrii@users.noreply.github.com>
2023-04-16 23:52:30 +0500

    Add files via upload

--- fyw-app/App.js
+++ fyw-app/App.js
@@ -9,82 +9,92 @@ import Login from "./components/login";
 import Signup from "./components/signup";
 import StudentDashboard from "./components/student-dashboard";
 import { firebase } from "./config";
+import Universities from "./components/universities";
+import MainPage from "./components/main_page";
+import Webinars from './components/webinars';
+// import Calendar from "./components/calendar";
+console.log(firebase);
 
 const Stack = createStackNavigator();
 
 function App() {
-	const [initializing, setInitializing] = useState(true);
-	const [user, setUser] = useState();
-	const [userType, setUserType] = useState(null);
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+  const [userType, setUserType] = useState(null);
 
-	function onAuthStateChanged(user) {
-		setUser(user);
-		if (initializing) setInitializing(false);
-		if (user) {
-			firebase
-				.firestore()
-				.collection("users")
-				.doc(user.uid)
-				.get()
-				.then((doc) => {
-					if (doc.exists) {
-						setUserType(doc.data().userType);
-					} else {
-						console.log("No such document!");
-					}
-				})
-				.catch((error) => {
-					console.log("Error getting document:", error);
-				});
-		}
-	}
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+    if (user) {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(user.uid)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            setUserType(doc.data().userType);
+          } else {
+            console.log("No such document!");
+          }
+        })
+        .catch((error) => {
+          console.log("Error getting document:", error);
+        });
+    }
+  }
 
-	useEffect(() => {
-		const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
-		return subscriber;
-	}, []);
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
 
-	const handleSignup = (email, password) => {
-		return firebase.auth().createUserWithEmailAndPassword(email, password);
-	};
+  const handleSignup = (email, password) => {
+    return firebase.auth().createUserWithEmailAndPassword(email, password);
+  };
 
-	if (initializing) return null;
+  if (initializing) return null;
 
-	if (!user) {
-		return (
-			<Stack.Navigator>
-				<Stack.Screen name="Login" component={Login} />
-				<Stack.Screen name="Signup">
-					{(props) => <Signup {...props} handleSignup={handleSignup} />}
-				</Stack.Screen>
-			</Stack.Navigator>
-		);
-	}
-	return (
-		<Stack.Navigator>
-			{userType === "Counsellor" ? (
-				<Stack.Screen name="CounsellorDashboard" component={CounsellorDashboard} />
-			) : (
-				<Stack.Screen name="StudentDashboard" component={StudentDashboard} />
-			)}
-			<Stack.Screen name="Logout" component={Login} />
-		</Stack.Navigator>
-	);
+  if (!user) {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={MainPage} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup">
+          {(props) => <Signup {...props} handleSignup={handleSignup} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    );
+  }
+  return (
+    <Stack.Navigator>
+      {userType === "Counsellor" ? (
+        <Stack.Screen name="Counsellor Dashboard" component={CounsellorDashboard} />
+      ) : (
+        <Stack.Screen name="Student Dashboard" component={StudentDashboard} />
+      )}
+      <Stack.Screen name="Universities" component={Universities} />
+      <Stack.Screen name="Webinars" component={Webinars} />
+      <Stack.Screen name="Logout" component={Login} />
+      
+      {/* <Stack.Screen name="Calendar" component={Calendar} /> */}
+    </Stack.Navigator>
+  );
 }
 
 export default () => {
-	return (
-		<NavigationContainer>
-			<App />
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		padding: 20,
-		justifyContent: "center"
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+    justifyContent: "center",
+  },
 });
